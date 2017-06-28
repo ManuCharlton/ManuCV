f6d188cbc4f54179ade5105a4c854e49632383e7
ManuCharlton <charltonmanu@gmail.com>
2017-06-28 15:33:44 +0200

    Delete head.php

--- constants/head.php
+++
@@ -1,8 +0,0 @@
-<?php include'../constants/constant.php'?>
-
-<head>
-    <meta charset="UTF-8">
-    <title>Homepage</title>
-    <link type="text/css" rel="stylesheet" href="<?php echo HOME?>css/styles.css">
-    <script src="<?php echo HOME?>jquery/jquery-3.2.0.min.js"></script>
-</head>

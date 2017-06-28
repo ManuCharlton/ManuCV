de172aaf386d7f26829ac0bd1a82e67357915dd7
ManuCharlton <charltonmanu@gmail.com>
2017-06-28 15:33:54 +0200

    Delete nav.php

--- constants/nav.php
+++
@@ -1,7 +0,0 @@
-<ul>
-    <li><big><a href="<?php echo HOME?>">Homepage</a></big></li>
-    <li><big><a href="<?php echo HOME?>pages/portfolio.php">Portfolio</a></big></li>
-    <li><big><a href="<?php echo HOME?>pages/contact.php">Contact</a></big></li>
-
-</ul>
-
